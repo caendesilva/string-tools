653226c59af8acb0cad4b17cac6dd90f94b9f0da
Caen De Silva <caen@desilva.se>
2024-06-27 12:06:01 +0200

    Improve test

--- test.php
+++ test.php
@@ -1,15 +1,25 @@
 <?php
 
-function test(string $command, string $input, string $expected): void
+function test(string $command, string $input, string $expected, bool $strict = true): void
 {
     $output = shell_exec("php ".__DIR__."/string-tools.php $command $input");
     $output = trim($output);
 
-    if ($output !== $expected) {
+    if ($strict) {
+        $passed = $output === $expected;
+    } else {
+        $passed = str_contains($output, $expected);
+    }
+
+    if (! $passed) {
         echo "Test failed for command: $command\n";
         echo "Expected: $expected\n";
         echo "Got: $output\n";
+    } else {
+        echo "Test passed for command: $command\n";
     }
 }
+
+test('help', '', 'String Tools CLI', false);
 
 test('kebab', 'Hello World', 'hello-world');
