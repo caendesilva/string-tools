9c855c23d0bba577dcf9e8f0329def6e1092a337
Caen De Silva <caen@desilva.se>
2024-06-27 12:02:41 +0200

    Trim output

--- test.php
+++ test.php
@@ -3,6 +3,7 @@
 function test(string $command, string $input, string $expected): void
 {
     $output = shell_exec("php ".__DIR__."/string-tools.php $command $input");
+    $output = trim($output);
 
     if ($output !== $expected) {
         echo "Test failed for command: $command\n";
